f48e5cd9ab79ea95b5518d3542f8048398026b6f
Marcus Leong <MLEONG@dstsystems.com>
2019-06-26 08:12:31 -0500

    Committing work from stackblitz.

---
+++ src/app/checkbox-overview-example.ts
@@ -0,0 +1,40 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Basic checkboxes
+ */
+
+@Component({
+  selector: 'checkbox-overview-example',
+  templateUrl: 'checkbox-overview-example.html',
+  styleUrls: ['checkbox-overview-example.css'],
+})
+
+export class CheckboxOverviewExample {
+
+  data = {
+    indeterminate: false,
+    checked: false,
+  };
+
+  data3 = {
+    checked: false,
+  };
+
+  approval() {
+    if (this.data.checked) {
+      this.data.indeterminate = true;
+    } else {
+      this.data.indeterminate = false;
+    }
+  }
+
+  isIndeterminate() {
+    return this.data.checked && !this.data.indeterminate;
+  }
+}
+
+
+/**  Copyright 2019 Google Inc. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
